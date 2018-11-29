4d37aa2904f3e88ddcf6a82fefbbd7b1086bf8e3
XiNGRZ <chenxingyu92@gmail.com>
2018-11-29 23:42:10 +0800

    colombo: Initial device tree
    Change-Id: Id14f5589b380aa68279a5818711176236eb48f95

---
+++ device.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2018 The MoKee Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+$(call inherit-product, vendor/smartisan/colombo/colombo-vendor.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+
+# Inherit from msm8996-common
+$(call inherit-product, device/smartisan/msm8996-common/common.mk)
---
+++ mokee.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2018 The MoKee Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from colombo device
+$(call inherit-product, device/smartisan/colombo/device.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mk.maintainer=XiNGRZ
+
+PRODUCT_NAME := mk_colombo
+PRODUCT_BRAND := SMARTISAN
+PRODUCT_DEVICE := colombo
+PRODUCT_MANUFACTURER := smartisan
+PRODUCT_MODEL := SM919
+
+PRODUCT_GMS_CLIENTID_BASE := android-smartisan
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="colombo" \
+    PRIVATE_BUILD_DESC="colombo-user 6.0.1 MXB48T 1 release-keys"
+
+BUILD_FINGERPRINT := SMARTISAN/colombo/colombo:6.0.1/MXB48T/1:user/release-keys
+
+# Sign bootable images
+PRODUCT_SUPPORTS_BOOT_SIGNER := true
+PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
+
+TARGET_VENDOR := Smartisan
